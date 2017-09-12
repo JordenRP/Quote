44cdeeecc1da23111d31fea736523d477da786ec
Pavel <gritsatsuev.pavel@gmail.com>
2017-09-12 23:08:46 +0400

    Js

---
+++ JS/js.js
@@ -0,0 +1,38 @@
+var color = [];
+var quote = '';
+var author = '';
+var port = '';
+var backg = '';
+
+var story = [
+  {
+    quote: "It's a sad thing not to have friends, but it is even sadder not to have enemies.",
+    autor: "Che Guevara",
+    port: "img/port/1.png",
+    backg: "img/back/1.png"
+  },
+  {
+    quote: "Never leave that till tomorrow which you can do today.",
+    autor: "Benjamin Franklin",
+    port: "img/port/2.png",
+    backg: "img/back/2.png"
+  },
+  {
+    quote: "Free speech carries with it some freedom to listen.",
+    autor: "Bob Marley",
+    port: "img/port/3.png",
+    backg: "img/back/3.png"
+  },
+  {
+    quote: "A friend is someone who gives you total freedom to be yourself.",
+    autor: "Jim Morrison",
+    port: "img/port/4.png",
+    backg: "img/back/4.png"
+  },
+  {
+    quote: "You only lie to two people in your life, your girlfriend and the police.",
+    autor: "Jack Nicholson",
+    port: "img/port/5.png",
+    backg: "img/back/5.png"
+  }
+]

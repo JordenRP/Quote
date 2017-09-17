9287bc874a82749d822f7ed09af9e889ec003613
Pavel <gritsatsuev.pavel@gmail.com>
2017-09-18 03:44:45 +0400

    End

--- JS/js.js
+++ JS/js.js
@@ -1,38 +1,45 @@
-var color = [];
-var quote = '';
-var author = '';
-var port = '';
-var backg = '';
+var color = ['#035024', '#F2C94C', '#B84646', '#452352', '#533316', '#103244', '#5F2828'];
 
-var story = [
-  {
-    quote: "It's a sad thing not to have friends, but it is even sadder not to have enemies.",
-    autor: "Che Guevara",
-    port: "img/port/1.png",
-    backg: "img/back/1.png"
-  },
-  {
-    quote: "Never leave that till tomorrow which you can do today.",
-    autor: "Benjamin Franklin",
-    port: "img/port/2.png",
-    backg: "img/back/2.png"
-  },
-  {
-    quote: "Free speech carries with it some freedom to listen.",
-    autor: "Bob Marley",
-    port: "img/port/3.png",
-    backg: "img/back/3.png"
-  },
-  {
-    quote: "A friend is someone who gives you total freedom to be yourself.",
-    autor: "Jim Morrison",
-    port: "img/port/4.png",
-    backg: "img/back/4.png"
-  },
-  {
-    quote: "You only lie to two people in your life, your girlfriend and the police.",
-    autor: "Jack Nicholson",
-    port: "img/port/5.png",
-    backg: "img/back/5.png"
-  }
-]
+  var quote_text = document.getElementById('quote-text');
+  var quote_autor = document.getElementById('quote-autor');
+  var port_img = document.getElementById('port_img');
+  var butt = document.getElementById('button');
+  var elem = document.querySelector('img');
+  var tweet = document.getElementById('tweet');
+
+ getObj(); 
+
+ function getObj () {
+   var xhr = new XMLHttpRequest ();
+   xhr.open('GET', 'https://api.myjson.com/bins/13buf1');
+   xhr.onload = function(evt) {
+     var rawData = evt.target.response;
+     loaderObj = JSON.parse(rawData);
+     
+     getQuote(loaderObj);
+     }
+   
+   xhr.send();
+ }
+
+
+function getQuote(sad) {
+  var randomQuote = Math.floor(Math.random() * 15);
+  var random = Math.floor(Math.random() * 7);
+  var currentColor = color[random];
+  var obj = sad[randomQuote];
+
+  document.body.firstElementChild.lastElementChild.className = obj.backg;
+  elem.setAttribute("src", obj.port);
+  quote_autor.innerHTML = obj.autor;
+  quote_text.innerHTML = obj.quote;
+  port_img.style.backgroundColor = currentColor;
+  butt.style.backgroundColor = currentColor;
+  
+  tweet.setAttribute('href', 'https://twitter.com/intent/tweet?hashtags=TwitterStories%2C&original_referer=https%3A%2F%2Fs.codepen.io%2Fboomerang%2FiFrameKey-2f1504bd-7bcc-f9cd-7645-05f4dbc2f18f%2Findex.html%3Feditors%3D1000&ref_src=twsrc%5Etfw&text='+obj.quote+'%20-'+obj.autor+'&tw_p=tweetbutton')
+};
+
+
+butt.addEventListener("click", getObj);
+
+  
